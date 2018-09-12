f6f492d518a319801c3c5dbe6f6d1c4658193ab8
Boon <boonps@gmail.com>
2018-09-12 10:46:50 +0700

    commit before send to github

--- winauto.py
+++ winauto.py
@@ -1,10 +1,27 @@
-from pywinauto import Desktop, Application
+from pywinauto import Desktop, Application, keyboard
 
-app = Application().Start(cmd_line=u'"D:\\Program Files\\Double Commander\\doublecmd.exe"')
-dclass = app.DClass
-dclass.Wait('ready')
-menu_item = dclass.MenuItem(u'C&onfiguration->&Options...')
-menu_item.Click()
+import time
 
-# app.Double_Commander.print_control_identifiers()
+app = Application(backend='uia').start("calc.exe")
+dlg = Desktop(backend="uia").Calculator
+dlg.type_keys('3')
+time.sleep(1)
+dlg.type_keys('/')
+time.sleep(1)
+dlg.type_keys('7')
+time.sleep(1)
+dlg.type_keys('=')
+txt = dlg.Static3.texts()
+txt = txt[0].split(' ')[2]
+
+app2 = Application(backend='uia').start("notepad.exe")
+time.sleep(1)
+app2.Untitled.edit.type_keys(txt)
+time.sleep(1)
+keyboard.SendKeys('{ENTER}')
+app2.Untitled.edit.type_keys(u'Automate{SPACE}any{SPACE}Windows{SPACE}Application')
+keyboard.SendKeys('{ENTER}')
+app2.Untitled.edit.type_keys(u'BY...{ENTER}Boon')
+
+
 
