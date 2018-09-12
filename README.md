b25ea60ab7f9cd6740e98121ce05b7c2e513e0f7
Boon <boonps@gmail.com>
2018-09-12 11:01:49 +0700

    update comment and readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# Another pywinauto example
+## Example Workflow
+1. Open Calculator.
+1. Press number and operator to do some calculation.
+1. Get calculation result.
+1. Open Notepad.
+1. Put result from Calculator to Notepad.
+
+This is so far from perfect but I hope every one can make this code better for any Python newbie.
+
+Thank you.
+
+Boon
--- winauto.py
+++ winauto.py
@@ -1,27 +1,34 @@
-from pywinauto import Desktop, Application, keyboard
+# Another pywinautu example
 
+from pywinauto import Desktop, Application, keyboard
 import time
 
+# Open Calculator in Windows 10 and do some calculation
 app = Application(backend='uia').start("calc.exe")
 dlg = Desktop(backend="uia").Calculator
 dlg.type_keys('3')
+# Add some delay between each key stroke
 time.sleep(1)
 dlg.type_keys('/')
 time.sleep(1)
 dlg.type_keys('7')
 time.sleep(1)
 dlg.type_keys('=')
+# Get calculation result
 txt = dlg.Static3.texts()
+# Get only the numeric value
 txt = txt[0].split(' ')[2]
 
+# Open Notepad from Windows 10
 app2 = Application(backend='uia').start("notepad.exe")
 time.sleep(1)
 app2.Untitled.edit.type_keys(txt)
 time.sleep(1)
+# All {key code} are self explained
 keyboard.SendKeys('{ENTER}')
 app2.Untitled.edit.type_keys(u'Automate{SPACE}any{SPACE}Windows{SPACE}Application')
 keyboard.SendKeys('{ENTER}')
-app2.Untitled.edit.type_keys(u'BY...{ENTER}Boon')
+app2.Untitled.edit.type_keys(u'BY...{ENTER}Python')
 
 
 
